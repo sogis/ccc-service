b9e343cd202c4fa23c227debb8b321cb0a469c94
bjsvwpfe <mpfeiffer1975@gmail.com>
2018-03-13 16:15:32 +0100

    Service Skeleton

---
+++ ccc-service/src/main/java/ch/so/agi/cccservice/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package ch.so.agi.cccservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.client.standard.WebSocketContainerFactoryBean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/myHandler").setAllowedOrigins("*");
+    }
+
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new MyHandler();
+    }
+
+    @Bean
+    public WebSocketContainerFactoryBean createWebSocketContainer() {
+        WebSocketContainerFactoryBean container = new WebSocketContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(8192);
+        container.setMaxBinaryMessageBufferSize(8192);
+        return container;
+    }
+
+}
