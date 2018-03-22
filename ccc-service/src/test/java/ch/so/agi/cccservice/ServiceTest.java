11e66343c3452ebd589f69f718eda9c10ff36377
barpastu <noemi.sturm@bd.so.ch>
2018-03-22 11:03:04 +0100

    testing path

---
+++ ccc-service/src/test/java/ch/so/agi/cccservice/ServiceTest.java
@@ -0,0 +1,48 @@
+package ch.so.agi.cccservice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ServiceTest {
+
+    @Test
+    public void appConnect() {
+    }
+
+    @Test
+    public void gisConnect() {
+    }
+
+    @Test
+    public void ready() {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void edit() {
+    }
+
+    @Test
+    public void show() {
+    }
+
+    @Test
+    public void cancel() {
+    }
+
+    @Test
+    public void changed() {
+    }
+
+    @Test
+    public void selected() {
+    }
+
+    @Test
+    public void dataWritten() {
+    }
+}
