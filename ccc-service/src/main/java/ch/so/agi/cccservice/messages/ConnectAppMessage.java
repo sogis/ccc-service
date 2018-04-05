19a2cb74682568a7980da317a80ab63c7bbbf907
barpastu <noemi.sturm@bd.so.ch>
2018-04-05 11:58:40 +0200

    Adding javadoc

--- ccc-service/src/main/java/ch/so/agi/cccservice/messages/ConnectAppMessage.java
+++ ccc-service/src/main/java/ch/so/agi/cccservice/messages/ConnectAppMessage.java
@@ -4,38 +4,71 @@ import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
 import ch.so.agi.cccservice.SessionId;
 
+/**
+ * Message which will be send from the application to start connection
+ */
 public class ConnectAppMessage extends AbstractMessage {
+
     public static final String METHOD_NAME = "connectApp";
+
     @JsonUnwrapped
-   private SessionId session;
-   private String clientName;
-   private String apiVersion;
+    private SessionId session;
+    private String clientName;
+    private String apiVersion;
 
-   public ConnectAppMessage() {
-      super(METHOD_NAME);
-   }
+    /**
+     * Constructor
+     */
+    public ConnectAppMessage() {
+        super(METHOD_NAME);
+    }
 
-   public SessionId getSession() {
-      return session;
-   }
 
-   public void setSession(SessionId session) {
-      this.session = session;
-   }
+    /**
+     * Gets SessionId delivered in message
+     * @return SessionId
+     */
+    public SessionId getSession() {
+        return session;
+    }
 
-   public String getClientName() {
-      return clientName;
-   }
+    /**
+     * Sets SessionId for message
+     * @param session as SessionId
+     */
+    public void setSession(SessionId session) {
+        this.session = session;
+    }
 
-   public void setClientName(String clientName) {
-      this.clientName = clientName;
-   }
+    /**
+     * Gets clientName delivered in message
+     * @return clientName as String
+     */
+    public String getClientName() {
+        return clientName;
+    }
 
-   public String getApiVersion() {
-      return apiVersion;
-   }
+    /**
+     * Sets clientName for message
+     * @param clientName as String
+     */
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
 
-   public void setApiVersion(String apiVersion) {
-      this.apiVersion = apiVersion;
-   }
+    /**
+     * Gets apiVersion delivered in message
+     * @return apiVersion as String
+     */
+    public String getApiVersion() {
+        return apiVersion;
+    }
+
+    /**
+     * Sets apiVersion for message
+     * @param apiVersion as String
+     */
+    public void setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
+    }
 }
--- ccc-service/src/main/java/ch/so/agi/cccservice/messages/ConnectGisMessage.java
+++ ccc-service/src/main/java/ch/so/agi/cccservice/messages/ConnectGisMessage.java
@@ -4,37 +4,69 @@ import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
 import ch.so.agi.cccservice.SessionId;
 
+/**
+ * Message which will be send from the GIS to start connection
+ */
 public class ConnectGisMessage extends AbstractMessage {
+
     public static final String METHOD_NAME = "connectGis";
+
     @JsonUnwrapped
     private SessionId session;
     private String clientName;
     private String apiVersion;
 
+    /**
+     * Constructor
+     */
     public ConnectGisMessage() {
         super(METHOD_NAME);
     }
 
+    /**
+     * Gets SessionId delivered in message
+     * @return SessionId
+     */
     public SessionId getSession() {
         return session;
     }
 
+    /**
+     * Sets SessionId for message
+     * @param session as SessionId
+     */
     public void setSession(SessionId session) {
         this.session = session;
     }
 
+    /**
+     * Gets clientName delivered in message
+     * @return clientName as String
+     */
     public String getClientName() {
         return clientName;
     }
 
+    /**
+     * Sets clientName for message
+     * @param clientName as String
+     */
     public void setClientName(String clientName) {
         this.clientName = clientName;
     }
 
+    /**
+     * Gets apiVersion delivered in message
+     * @return apiVersion as String
+     */
     public String getApiVersion() {
         return apiVersion;
     }
 
+    /**
+     * Sets apiVersion for message
+     * @param apiVersion as String
+     */
     public void setApiVersion(String apiVersion) {
         this.apiVersion = apiVersion;
     }
