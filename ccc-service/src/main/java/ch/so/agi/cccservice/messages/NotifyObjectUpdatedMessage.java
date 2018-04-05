c174ffe2bc261070dd092f9f1c01a5fe99cf5fa2
barpastu <noemi.sturm@bd.so.ch>
2018-04-05 12:23:01 +0200

    adding javadoc

--- ccc-service/src/main/java/ch/so/agi/cccservice/messages/NotifyObjectUpdatedMessage.java
+++ ccc-service/src/main/java/ch/so/agi/cccservice/messages/NotifyObjectUpdatedMessage.java
@@ -2,18 +2,32 @@ package ch.so.agi.cccservice.messages;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+/**
+ * Message sent from Application on changed Objects
+ */
 public class NotifyObjectUpdatedMessage extends AbstractMessage {
     public static final String METHOD_NAME = "notifyObjectUpdated";
     private JsonNode properties;
 
+    /**
+     * Constructor
+     */
     public NotifyObjectUpdatedMessage() {
         super(METHOD_NAME);
     }
 
+    /**
+     * Gets changed object (properties)
+     * @return properties as Json
+     */
     public JsonNode getProperties() {
         return properties;
     }
 
+    /**
+     * Sets changed object in message
+     * @param properties as Json
+     */
     public void setProperties(JsonNode properties) {
         this.properties = properties;
     }
