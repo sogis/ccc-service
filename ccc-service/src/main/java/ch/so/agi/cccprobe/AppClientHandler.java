bd872d89e0f83753e72f40612931db4a2c6695ee
bjsvwsch <martin.schweizer@bd.so.ch>
2018-04-05 08:18:46 +0200

    new Version of SimpleClient

--- ccc-service/src/main/java/ch/so/agi/cccprobe/AppClientHandler.java
+++ ccc-service/src/main/java/ch/so/agi/cccprobe/AppClientHandler.java
@@ -1,10 +1,11 @@
 package ch.so.agi.cccprobe;
 
 import ch.so.agi.cccservice.JsonConverter;
+import ch.so.agi.cccservice.ServiceException;
 import ch.so.agi.cccservice.SessionId;
 import ch.so.agi.cccservice.messages.AbstractMessage;
-import ch.so.agi.cccservice.messages.AppConnectMessage;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.socket.*;
 
 public class AppClientHandler implements WebSocketHandler {
@@ -34,12 +35,26 @@ public class AppClientHandler implements WebSocketHandler {
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
 
-        System.out.println("AppMessage received: " + message.getPayload());
-        if (appReady == null && message.getPayload().equals("{\"method\":\"ready\",\"apiVersion\":\"1.0\"}")) {
+        //System.out.println("AppMessage received: " + message.getPayload());
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode obj = mapper.readTree(message.getPayload().toString());
+        String method;
+        try {
+            method = obj.get("method").asText();
+        } catch (NullPointerException e) {
+            throw new ServiceException(400, "No method found in given JSON");
+        }
+
+        if (method.equals("ready")) {
             System.out.println("AppConnection ready");
             appReady = true;
         }
+        else if (method.equals("error")) {
+            System.out.println("Got Error: "+obj.get("message").asText());
+        }
         else {
+            System.out.println("Did not get correct message. Got: "+message.getPayload());
             appReady = false;
         }
     }
@@ -57,7 +72,16 @@ public class AppClientHandler implements WebSocketHandler {
     public void sendMessage(AbstractMessage msg) throws Exception {
         JsonConverter jsonConverter = new JsonConverter();
         String resultingJson = jsonConverter.messageToString(msg);
-
         webSocketSession.sendMessage(new TextMessage(resultingJson));
+    }
+
+    public boolean isConnected(WebSocketHandler webSocketHandler) {
+        try {
+            webSocketSession.isOpen();
+            return true;
+        } catch(NullPointerException e){
+            System.out.println("WebsocketSession is not open!");
+            return false;
+        }
     }
 }
--- ccc-service/src/main/java/ch/so/agi/cccprobe/SimpleClient.java
+++ ccc-service/src/main/java/ch/so/agi/cccprobe/SimpleClient.java
@@ -1,16 +1,8 @@
 package ch.so.agi.cccprobe;
 
-import java.util.Scanner;
-
-import ch.so.agi.cccservice.JsonConverter;
 import ch.so.agi.cccservice.SessionId;
 import ch.so.agi.cccservice.messages.AppConnectMessage;
 import ch.so.agi.cccservice.messages.GisConnectMessage;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketMessage;
-import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
 public class SimpleClient {
@@ -24,12 +16,18 @@ public class SimpleClient {
         appClient.doHandshake(appSessionHandler,"ws://localhost:8080/myHandler");
 
         AppConnectMessage appConnectMessage = new AppConnectMessage();
-        appConnectMessage.setApiVersion("1.0");
+        appConnectMessage.setApiVersion("2.0");
         appConnectMessage.setSession(sessionId);
         appConnectMessage.setClientName("Axioma Mandant AfU");
 
         Thread.sleep(2000);
-        appSessionHandler.sendMessage(appConnectMessage);
+        if (appSessionHandler.isConnected(appSessionHandler)) {
+            appSessionHandler.sendMessage(appConnectMessage);
+        }
+        else {
+            System.out.println("Session not open. Could not send appConnectMessage");
+        }
+
 
         StandardWebSocketClient gisClient = new StandardWebSocketClient();
         AppClientHandler gisSessionHandler = new AppClientHandler();
@@ -42,12 +40,18 @@ public class SimpleClient {
         gisConnectMessage.setClientName("Gis Client");
 
         Thread.sleep(2000);
-        gisSessionHandler.sendMessage(gisConnectMessage);
+        if (gisSessionHandler.isConnected(gisSessionHandler)) {
+            gisSessionHandler.sendMessage(gisConnectMessage);
+        }
+        else {
+            System.out.println("Session not open. Could not send gisConnectMessage");
+        }
 
         Thread.sleep(2000);
         if (gisSessionHandler.getAppReady() != null && gisSessionHandler.getAppReady() == true && appSessionHandler.getAppReady() != null && appSessionHandler.getAppReady() == true) {
             System.exit(0);
         }
+
         System.exit(1);
 
     }
