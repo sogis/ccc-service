07839476234daf4f20e4db1c8f5aaac46fb24e4e
barpastu <noemi.sturm@bd.so.ch>
2018-04-05 12:31:54 +0200

    adding javadoc

--- ccc-service/src/main/java/ch/so/agi/cccservice/messages/ShowGeoObjectMessage.java
+++ ccc-service/src/main/java/ch/so/agi/cccservice/messages/ShowGeoObjectMessage.java
@@ -2,27 +2,50 @@ package ch.so.agi.cccservice.messages;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+/**
+ * Message sent from Application on order to show object
+ */
 public class ShowGeoObjectMessage extends AbstractMessage {
+
     public static final String METHOD_NAME = "showGeoObject";
     private JsonNode context;
     private JsonNode data;
 
+    /**
+     * Constructor
+     */
     public ShowGeoObjectMessage() {
         super(METHOD_NAME);
     }
 
+    /**
+     * Gets affected object (context)
+     * @return context as Json
+     */
     public JsonNode getContext() {
         return context;
     }
 
+    /**
+     * Sets affected object in message
+     * @param context as Json
+     */
     public void setContext(JsonNode context) {
         this.context = context;
     }
 
+    /**
+     * Gets position (Data)
+     * @return Data as Json
+     */
     public JsonNode getData() {
         return data;
     }
 
+    /**
+     * Sets position (Data) in message
+     * @param data as Json
+     */
     public void setData(JsonNode data) {
         this.data = data;
     }
