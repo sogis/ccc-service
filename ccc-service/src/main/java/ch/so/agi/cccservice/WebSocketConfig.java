710740f3e3d6e9b4390aeea9d001b9a58343625d
claeis <ce@eisenhutinformatik.ch>
2018-04-14 08:47:52 +0200

    make url self-describing (fixes #1)

--- ccc-service/src/main/java/ch/so/agi/cccservice/WebSocketConfig.java
+++ ccc-service/src/main/java/ch/so/agi/cccservice/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(socketHandler, "/myHandler").setAllowedOrigins("*");
+        registry.addHandler(socketHandler, "/ccc-service").setAllowedOrigins("*");
     }
 
     @Bean
