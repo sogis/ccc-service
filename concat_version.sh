70d164848af74e0eeeb6a383e12ad4557dfc6a80
uncoyote <jeker.git@outlook.de>
2021-02-03 09:13:39 +0100

    using org secrets

---
+++ concat_version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Concats the full version and prints it to the stdout
+
+prefix='1.1.'
+
+suffix=$GITHUB_BUILD_NUMBER
+if [ -n suffix ]; then
+  suffix='localbuild'
+fi
+
+echo $prefix$suffix
+
+
+
+
