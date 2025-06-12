dba87a07c02e0659717850c3e331da29abbff5c6
Christian Baumann <christian.baumann@bd.so.ch>
2025-06-12 15:06:10 +0200

    enabled print out of stacktrace

--- src/main/java/ch/so/agi/cccservice/BackgroundService.java
+++ src/main/java/ch/so/agi/cccservice/BackgroundService.java
@@ -5,14 +5,14 @@ import org.slf4j.LoggerFactory;
 
 /** runs back ground tasks of the ccc.service.
  * Closes inactive sessions and sends heart beats (ping) to not so active sessions.
- * 
+ *
  */
 public class BackgroundService implements Runnable {
     private Logger logger = LoggerFactory.getLogger(BackgroundService.class);
     private SessionPool sessionPool;
     private long maxInactivityTime;
     private long pingIntervalTime;
-    
+
     public BackgroundService(SessionPool sessionPool, long maxInactivityTime,long pingIntervalTime){
         this.sessionPool = sessionPool;
         this.maxInactivityTime=maxInactivityTime;
@@ -29,10 +29,11 @@ public class BackgroundService implements Runnable {
             synchronized(sessionPool){
                 sessionPool.closeInactiveSessions(maxInactivityTime);
             }
-            
+
         }
         catch (Exception e){
-            logger.info("Exception!! " +e);
+            logger.info("Exception!! " + e);
+            logger.debug("  details: ", e);
         }
     }
 
